80fcf5ea43a37275a91f0e6a68f70d9abe06c52e
Swapnil Rawat <rawatswapnil911@gmail.com>
2020-05-22 20:16:01 +0530

    added files

---
+++ app/src/main/java/com/ibm/mysampleapp/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.ibm.mysampleapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Physics");
+    }
+
+    public void kinClick(View view) {
+        Intent i = new Intent(this, Main5Activity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/ibm/mysampleapp/Main4Activity.java
@@ -0,0 +1,21 @@
+package com.ibm.mysampleapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Mathematics");
+    }
+
+}
